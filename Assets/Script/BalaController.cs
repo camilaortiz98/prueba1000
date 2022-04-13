e335e8343a37eb34c224b69514691be9f9cbafe7
Camila Ortiz <cami089844@gmil.com>
2022-04-12 21:36:20 -0500

    ar

---
+++ Assets/Script/BalaController.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BalaController : MonoBehaviour
+{
+    public float Speed = 4.5f;
+    private Rigidbody2D rb;
+    private void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+
+        Destroy(this.gameObject,5);
+    }
+    // Update is called once per frame
+    void Update()
+    {
+
+        rb.velocity = new Vector2(Speed, rb.velocity.y);
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        var tag = collision.gameObject.tag;
+        if (tag == "Enemy")
+        {
+            Destroy(this.gameObject);
+            Destroy(collision.gameObject);
+        }
+    }
+
+
+}
---
+++ Assets/Script/DinoController.cs
@@ -0,0 +1,85 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DinoController : MonoBehaviour
+{
+    public float velocityX = 5;
+    public float jumpForce = 100;
+
+    public GameObject BalaPrefab;
+
+    // Start is called before the first frame update
+
+    private Rigidbody2D rb;
+    private Animator animator;
+    private SpriteRenderer sr;
+
+    //constantes
+
+    private const int WALK = 0;
+    private const int JUMP = 1;
+
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        animator = GetComponent<Animator>();
+        sr = GetComponent<SpriteRenderer>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+
+        if (Input.GetKey(KeyCode.RightArrow))
+        {
+            rb.velocity = new Vector2(velocityX, rb.velocity.y);
+            sr.flipX = false;
+            changeAnimation(WALK);
+
+        }
+
+        if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            rb.velocity = new Vector2(-velocityX, rb.velocity.y);
+            sr.flipX = true;
+            changeAnimation(WALK);
+
+        }
+
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            rb.AddForce(Vector2.up * jumpForce, ForceMode2D.Impulse);
+            changeAnimation(JUMP);
+        }
+   
+
+        if (Input.GetKeyUp(KeyCode.X))
+        {
+            Disparar();
+        }
+
+    }
+
+    private void Disparar()
+    {
+        var x = this.transform.position.x;
+        var y = this.transform.position.y;
+
+        var bulletGO = Instantiate(BalaPrefab, new Vector2(x, y), Quaternion.identity) as GameObject;
+        if (sr.flipX)
+        {
+            var controller = bulletGO.GetComponent<BalaController>();
+            controller.Speed *= -1;
+
+        }
+       
+    }
+    
+    private void changeAnimation(int animation)
+    {
+        animator.SetInteger("Estado", animation);
+    }
+}
